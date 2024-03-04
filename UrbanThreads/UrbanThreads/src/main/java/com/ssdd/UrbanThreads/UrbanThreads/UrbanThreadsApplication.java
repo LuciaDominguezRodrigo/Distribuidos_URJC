dc51bb25fbe78802805c4859a522eff874e2e3f1
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2024-03-04 11:20:06 +0100

    Proyect created
    The prpyect is empty. Classes and files will be added as needed

---
+++ UrbanThreads/UrbanThreads/src/main/java/com/ssdd/UrbanThreads/UrbanThreads/UrbanThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.ssdd.UrbanThreads.UrbanThreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanThreadsApplication.class, args);
+	}
+
+}
---
+++ UrbanThreads/UrbanThreads/src/test/java/com/ssdd/UrbanThreads/UrbanThreads/UrbanThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssdd.UrbanThreads.UrbanThreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
