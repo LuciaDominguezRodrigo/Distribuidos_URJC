ef7c8c120991bd2e193b5732598cc6e53b8f05e6
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2024-03-06 17:55:01 +0100

    Mustache template format for AJAX integration
    Event main structure has been moved apart to another Mustache template to unify event structure in main screen.

--- UrbanThreads/UrbanThreads/src/main/java/com/ssdd/UrbanThreads/UrbanThreads/controllers/ProductWebController.java
+++ UrbanThreads/UrbanThreads/src/main/java/com/ssdd/UrbanThreads/UrbanThreads/controllers/ProductWebController.java
@@ -1,27 +1,17 @@
 package com.ssdd.UrbanThreads.UrbanThreads.controllers;
 
 
-import com.ssdd.UrbanThreads.UrbanThreads.entities.Product;
 import com.ssdd.UrbanThreads.UrbanThreads.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
-import java.sql.Blob;
 
 
 @Controller
-public class ProductController {
+public class ProductWebController {
 
     @Autowired
     private ProductService productService;
@@ -30,7 +20,7 @@ public class ProductController {
     @GetMapping("/")
     public String index(Model model) {
         // Obtener todos los productos de la base de datos
-        model.addAttribute("productos", productService.getAllProducts());
+        model.addAttribute("products", productService.getAllProducts());
         return "index";
     }
 
