eb82003dff8b6fac3e83d792f8234aacf5028755
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2024-03-07 16:35:11 +0100

    Alert confirmation for deleting created

--- UrbanThreads/UrbanThreads/src/main/resources/static/js/productDetails.js
+++ UrbanThreads/UrbanThreads/src/main/resources/static/js/productDetails.js
@@ -2,25 +2,37 @@ function checkStock(xs, s, m, l, xl, xxl) {
     let selectedSize = document.getElementById("sizeSelect").value;
     let selectedSizeQuantity = 0;
     switch (selectedSize) {
-        case 'XS': selectedSizeQuantity = xs;break;
-        case 'S': selectedSizeQuantity = s;break;
-        case 'M': selectedSizeQuantity = m;break;
-        case 'L': selectedSizeQuantity = l;break;
-        case 'XL': selectedSizeQuantity = xl;break;
-        case 'XXL': selectedSizeQuantity = xxl;break;
-        break;
+        case 'XS':
+            selectedSizeQuantity = xs;
+            break;
+        case 'S':
+            selectedSizeQuantity = s;
+            break;
+        case 'M':
+            selectedSizeQuantity = m;
+            break;
+        case 'L':
+            selectedSizeQuantity = l;
+            break;
+        case 'XL':
+            selectedSizeQuantity = xl;
+            break;
+        case 'XXL':
+            selectedSizeQuantity = xxl;
+            break;
+            break;
     }
 
     let quantityField = document.getElementById("quantity");
     quantityField.max = selectedSizeQuantity;
-    if(quantityField.value > selectedSizeQuantity){
+    if (quantityField.value > selectedSizeQuantity) {
         quantityField.value = selectedSizeQuantity;
     }
 
     let availabilityTag = document.getElementById("availability");
     let addToCartButton = document.getElementById("addToCart")
 
-    if (selectedSizeQuantity > 0){
+    if (selectedSizeQuantity > 0) {
         availabilityTag.innerText = ("En stock. Quedan " + selectedSizeQuantity + " unidades");
 
         availabilityTag.classList.remove("d-none");
@@ -29,8 +41,7 @@ function checkStock(xs, s, m, l, xl, xxl) {
 
         addToCartButton.classList.remove("d-none");
         addToCartButton.classList.add("d-block");
-    }
-    else{
+    } else {
         availabilityTag.innerText = ("No disponible en stock");
 
         availabilityTag.classList.remove("d-none");
@@ -39,6 +50,19 @@ function checkStock(xs, s, m, l, xl, xxl) {
 
         addToCartButton.classList.remove("d-block");
         addToCartButton.classList.add("d-none");
+    }
+
+}
+
+function confirmDelete() {
+    var confirmation = confirm("¿deseas eliminar este producto?");
+
+    if (confirmation) {
+        alert("El producto ha sido eliminado");
+        return true;
+    } else {
+        alert("El producto no ha sido eliminado");
+        return false;
     }
 
 }
