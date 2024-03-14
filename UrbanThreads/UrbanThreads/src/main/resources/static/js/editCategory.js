726445a9cb457380021e3964c82397d71c75f19e
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2024-03-14 09:28:00 +0100

    Resume the function for color display

--- UrbanThreads/UrbanThreads/src/main/resources/static/js/editCategory.js
+++ UrbanThreads/UrbanThreads/src/main/resources/static/js/editCategory.js
@@ -14,20 +14,7 @@ $(document).ready(function() {
     });
 });
 
+
 document.getElementById('categoryColor').addEventListener('input', function() {
     let rgbColor = this.value.match(/\d+/g);
-    document.getElementById('hexColor').value = rgbToHex(rgbColor[0], rgbColor[1], rgbColor[2]);
-});
-
-// Function to convert RGB to Hex
-function rgbToHex(r, g, b) {
-    r = Math.round(parseInt(r)).toString(16);
-    g = Math.round(parseInt(g)).toString(16);
-    b = Math.round(parseInt(b)).toString(16);
-
-    if (r.length === 1) r = "0" + r;
-    if (g.length === 1) g = "0" + g;
-    if (b.length === 1) b = "0" + b;
-
-    return "#" + r + g + b;
-}
+})
