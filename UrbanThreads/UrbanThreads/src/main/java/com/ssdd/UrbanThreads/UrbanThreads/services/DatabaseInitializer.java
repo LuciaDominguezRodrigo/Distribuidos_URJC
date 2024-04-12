1f6b064859ff1bf5027e9f1ec60a26fa84fe403a
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2024-04-12 12:13:07 +0200

    Revert "Started models for classes needed for ddbb transition"
    This reverts commit a03d5dad7627c0b2a31b25216d4688ca7ed61997.

--- UrbanThreads/UrbanThreads/src/main/java/com/ssdd/UrbanThreads/UrbanThreads/services/DatabaseInitializer.java
+++ UrbanThreads/UrbanThreads/src/main/java/com/ssdd/UrbanThreads/UrbanThreads/services/DatabaseInitializer.java
@@ -1,49 +1,29 @@
 package com.ssdd.UrbanThreads.UrbanThreads.services;
 
 
-import com.ssdd.UrbanThreads.UrbanThreads.entities.DCategory;
-import com.ssdd.UrbanThreads.UrbanThreads.entities.DProduct;
-import com.ssdd.UrbanThreads.UrbanThreads.entities.DOrder;
-import com.ssdd.UrbanThreads.UrbanThreads.repository.*;
+import com.ssdd.UrbanThreads.UrbanThreads.repository.CategoryRepository;
+import com.ssdd.UrbanThreads.UrbanThreads.repository.OrderRepository;
+import com.ssdd.UrbanThreads.UrbanThreads.repository.ProductRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
 @Service
 public class DatabaseInitializer {
     @Autowired
-    private DCategoryRepository categoryRepository;
+    private CategoryRepository categoryRepository;
 
     @Autowired
-    private DOrderRepository orderRepository;
+    private OrderRepository orderRepository;
 
     @Autowired
-    private DProductRepository productRepository;
+    private ProductRepository productRepository;
 
 
     @PostConstruct
     public void init() {
-        // Inicializar algunas categorías
-        DCategory category1 = new DCategory("Category 1");
-        DCategory category2 = new DCategory("Category 2");
-        categoryRepository.save(category1);
-        categoryRepository.save(category2);
-
-        // Inicializar algunos productos asociados a las categorías
-        DProduct product1 = new DProduct("Product 1", category1);
-        DProduct product2 = new DProduct("Product 2", category2);
-        productRepository.save(product1);
-        productRepository.save(product2);
-
-        // Inicializar una orden con algunos elementos de pedido
-        DOrder order = new DOrder(new Date());
-        Set<DOrder> orderItems = new HashSet<>();
-        order.setOrderItems(orderItems);
-        orderRepository.save(order);
-    }
 
     }
+
+
+}
